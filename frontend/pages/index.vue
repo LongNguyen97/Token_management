22406dab94d1f48298e6faa18711e7c152893eb9
LongNguyen97 <vietlong.tnq@gmaoil.com>
2023-01-08 21:55:57 +0700

    init

---
+++ frontend/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-main>
+    <v-alert
+      dense
+      elevation="10"
+      prominent
+      type="success"
+    >saDs</v-alert>
+  </v-main>
+</template>
+
+<script>
+import Login from "~/pages/login";
+
+export default {
+  name: 'IndexPage',
+  components: {Login}
+}
+</script>
---
+++ frontend/pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- Must have the app property -->
+
+
+</template>
+
+<script>
+
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+// import { Button, Input, Space } from 'antd';
+export default {
+  name: "login"
+}
+</script>
+
+
+<style scoped>
+
+</style>
